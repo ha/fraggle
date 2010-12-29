310db020082119a229179f983ece08c20e56ba96
Blake Mizerany <blake.mizerany@gmail.com>
2010-12-28 23:12:04 -0600

    encoder

--- lib/fraggel.rb
+++ lib/fraggel.rb
@@ -128,6 +128,30 @@ module Fraggel
 
   end
 
+  module Encoder
+
+    CRLF = "\r\n"
+
+    def encode(value)
+      case value
+      when nil
+        "$-1\r\n"
+      when true
+        encode(1)
+      when false
+        encode(0)
+      when Integer
+        ":%d\r\n" % [value]
+      when String
+        "$%d\r\n%s\r\n" % [value.length, value]
+      when Array
+        mapped = value.map {|x| encode(x) }
+        "*%d\r\n%s" % [mapped.length, mapped]
+      when StandardError, Exception
+        "-ERR: %s\r\n" % [value.message]
+      end
+    end
+  end
 
   module Responder
 
