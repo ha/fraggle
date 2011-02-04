bdc5e1807081c298596278950e9520e38651504b
Blake Mizerany <blake.mizerany@gmail.com>
2011-02-03 20:37:45 -0800

    walk example

---
+++ example/walk.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'eventmachine'
+require 'fraggle'
+
+EM.run do
+
+  EM.error_handler do |e|
+    $stderr.puts e.message + "\n" + (e.backtrace * "\n")
+  end
+
+  c = Fraggle.connect "doozer://127.0.0.1:8046"
+
+  paths = []
+  req = c.walk "/**" do |e|
+    paths << e.path+"="+e.value
+  end
+
+  req.done do
+    puts *paths
+  end
+
+
+end
