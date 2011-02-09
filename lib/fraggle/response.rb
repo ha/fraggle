dac8a15edf53d776eff6722b2ec9ed86aa392463
Blake Mizerany <blake.mizerany@gmail.com>
2011-02-08 20:59:00 -0800

    correct repsonse sugar

--- lib/fraggle/response.rb
+++ lib/fraggle/response.rb
@@ -14,20 +14,28 @@ module Fraggle
     Dir     = -2
     Dummy   = -3
 
+    Refused = -1
+
     # CAS
     def missing?  ; cas == Missing ; end
     def dir?      ; cas == Dir     ; end
     def dummy?    ; cas == Dummy   ; end
 
+    def del?      ; missing?       ; end
+    def set?      ; !del?          ; end
+
     # ERR
-    def ok?           ; error_code != 0                 ; end
-    def other?        ; error_code == Err::OTHER        ; end
-    def unknown_verb? ; error_code == Err::UNKNOWN_VERB ; end
-    def redirect?     ; error_code == Err::REDIRECT     ; end
-    def invalid_snap? ; error_code == Err::INVALID_SNAP ; end
-    def mismatch?     ; error_code == Err::CAS_MISMATCH ; end
-    def not_dir?      ; error_code == Err::NOT_DIR      ; end
-    def is_dir?       ; error_code == Err::ISDIR        ; end
+    def ok?           ; err_code != 0                 ; end
+    def other?        ; err_code == Err::OTHER        ; end
+    def unknown_verb? ; err_code == Err::UNKNOWN_VERB ; end
+    def redirect?     ; err_code == Err::REDIRECT     ; end
+    def invalid_snap? ; err_code == Err::INVALID_SNAP ; end
+    def mismatch?     ; err_code == Err::CAS_MISMATCH ; end
+    def not_dir?      ; err_code == Err::NOT_DIR      ; end
+    def is_dir?       ; err_code == Err::ISDIR        ; end
+
+    # Custom
+    def disconnected? ; err_code == Errno::ECONNREFUSED::Errno ; end
   end
 
 end
