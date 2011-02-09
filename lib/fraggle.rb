22a80d156e79c0e7f7a47b6b03092f6f34b49d1b
Blake Mizerany <blake.mizerany@gmail.com>
2011-02-08 22:44:00 -0800

    impl doozer uri spec

--- lib/fraggle.rb
+++ lib/fraggle.rb
@@ -4,8 +4,22 @@ require 'uri'
 module Fraggle
 
   def self.connect(uri, *args)
-    uri = URI(uri)
-    c = EM.connect(uri.host, uri.port, Client, uri, *args)
+    params = uri.gsub(/^doozer:\?/, '').split("&")
+    addrs  = []
+
+    params.each do |param|
+      k, v = param.split("=")
+      if k == "ca" && v =~ /^[\d.]+:\d+$/
+        addrs << v
+      end
+    end
+
+    if addrs.empty?
+      fail "The URI (#{uri}) does not contain valid addresses"
+    end
+
+    host, port = addrs.first.split(":")
+    c = EM.connect(host, port, Client, addrs, *args)
     Snap.new(0, c)
   end
 
