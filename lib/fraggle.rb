970bc635a307de5f5a5fe3488c57b56afc90a885
Blake Mizerany <blake.mizerany@gmail.com>
2011-02-10 12:28:55 -0800

    damn it

--- lib/fraggle.rb
+++ lib/fraggle.rb
@@ -7,20 +7,7 @@ module Fraggle
   extend Logger
 
   def self.connect(uri, *args)
-    params = uri.gsub(/^doozer:\?/, '').split("&")
-    addrs  = []
-
-    params.each do |param|
-      k, v = param.split("=")
-      if k == "ca" && v =~ /^[\d.]+:\d+$/
-        addrs << v
-      end
-    end
-
-    if addrs.empty?
-      fail "The URI (#{uri}) does not contain valid addresses"
-    end
-
+    addrs = addrs_for(uri)
     host, port = addrs.first.split(":")
     c = EM.connect(host, port, Client, addrs, *args)
     Snap.new(0, c)
