b82d802a983bb65c7f85f300e23f0c55fca5936e
Blake Mizerany <blake.mizerany@gmail.com>
2011-03-28 19:29:34 -0700

    parse uri

--- lib/fraggle.rb
+++ lib/fraggle.rb
@@ -1,3 +1,18 @@
 module Fraggle
-  # Your code goes here...
+
+  def self.uri(u)
+    if u =~ /^doozerd:\?(.*)$/
+      parts = $1.split("&")
+      parts.inject([]) do |m, pt|
+        k, v = pt.split("=")
+        if k == "ca"
+          m << v
+        end
+        m
+      end
+    else
+      raise ArgumentError, "invalid doozerd uri"
+    end
+  end
+
 end
