b4f265073fe8319c9fbfdeba873118cf3597f0b0
Blake Mizerany <blake.mizerany@gmail.com>
2011-04-20 17:11:03 -0500

    update redirect example

--- example/redirect.rb
+++ example/redirect.rb
@@ -1,17 +1,22 @@
 require 'rubygems'
 require 'fraggle'
 
+# This example assumes you have a Doozer with CAL privledges listening on port
+# 8041 and a Doozer sink listening on 8042.  This example will connect to the
+# sink and attempt a write operation that will be redirected by the sink.
+# Fraggle will pick the only remaining addr it has which is the CAL Doozer and
+# retry the write operation.
+
 EM.run do
   # Connect to a slave (be sure this is a slave)
   c = Fraggle.connect("doozer:?ca=127.0.0.1:8042&ca=127.0.0.1:8041")
   c.log.level = Logger::DEBUG
 
   # Wait for rev 100,000
-  c.set(-1, "/foo", "bar") do |e|
-    p ["This should not be!", e]
+  c.set("/foo", "bar", -1) do |e|
+    p [:valid, e]
   end.error do |e|
+    # This shouldn't happen
     p [:err, e]
-  end.done do
-    p [:done, "This should not be!"]
   end
 end
