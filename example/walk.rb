9d4e620308b9c770d9dc006f054ca3e80e43d673
Blake Mizerany <blake.mizerany@gmail.com>
2011-04-21 18:34:48 -0500

    better WALK example

--- example/walk.rb
+++ example/walk.rb
@@ -3,21 +3,27 @@ require 'eventmachine'
 require 'fraggle'
 
 EM.run do
-
-  EM.error_handler do |e|
-    $stderr.puts e.message + "\n" + (e.backtrace * "\n")
-  end
-
   c = Fraggle.connect
 
   paths = []
-  req = c.walk "/**" do |e|
+
+  req = c.walk("/**", 1)
+
+  valid = Proc.new do |e|
     paths << e.path+"="+e.value
   end
 
-  req.done do
+  done = Proc.new do
+    puts "rev #{req.rev} " + ("-"*25)
     puts *paths
+
+    paths.clear
+
+    req = c.walk("/**", req.rev+1)
+    req.valid(&valid)
+    req.done(&done)
   end
 
-
+  req.valid(&valid)
+  req.done(&done)
 end
