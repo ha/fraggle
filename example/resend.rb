37db459d1cc0cf55df36f3870351de2a7e3024a9
Blake Mizerany <blake.mizerany@gmail.com>
2011-04-27 14:37:25 -0700

    fix example/resend

--- example/resend.rb
+++ example/resend.rb
@@ -9,13 +9,14 @@ require 'fraggle'
 #
 EM.run do
   c = Fraggle.connect
+  c.log.level = Logger::DEBUG
 
   puts "# To see this work, kill the doozer this is connected to"
   puts "# to see it resend the request to the next connection."
   puts
 
   # Wait for rev 100,000
-  c.get(100_000, "/foo") do |e|
+  c.get("/foo", 100_000) do |e|
     p ["This should not be!", e]
   end.error do |e|
     p [:err, e]
