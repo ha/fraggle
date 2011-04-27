5feb97dc5e79d9072cc16e67000588c17f07f87b
Blake Mizerany <blake.mizerany@gmail.com>
2011-04-27 15:02:55 -0700

    update getdir example

--- example/getdir.rb
+++ example/getdir.rb
@@ -4,16 +4,55 @@ require 'fraggle'
 
 EM.run do
 
+  ## NOTE:  This will be easier the very near future.
+
+  ## Collects names in a directory
+
   c = Fraggle.connect
   c.log.level = Logger::DEBUG
 
-  ents = []
-  c.getdir("/") do |e|
+  def c.getdirx(path, rev, offset, &blk)
+    w = Request.new
+    w.valid(&blk)
+
+    err = Proc.new do |e|
+      if e.err_code == Fraggle::Response::Err::AGAIN
+        w.emit(:done)
+      else
+        w.emit(:error, e)
+      end
+    end
+
+    req = getdir(path, rev, offset) do |e|
+      blk.call(e)
+      req = getdirx(path, rev, offset+1, &blk)
+      req.error(&err)
+    end
+    req.error(&err)
+
+    w
+  end
+
+  ents, n = [], 0
+
+  f = Proc.new do |e|
     ents << e.path
-  end.done do
-    p [:ents, ents]
-  end.error do |e|
-    raise StandardError.new("err: "+e.inspect)
+    n += 1
+    c.getdir("/ctl", nil, n, &f).error do |e|
+      if e.err_code == Fraggle::Response::Err::RANGE
+        p [:ents, ents]
+      else
+        p [:err, e]
+      end
+    end
+  end
+
+  c.getdir("/ctl", nil, n, &f).error do |e|
+    if e.err_code == Fraggle::Response::Err::RANGE
+      p [:ents, ents]
+    else
+      p [:err, e]
+    end
   end
 
 end
