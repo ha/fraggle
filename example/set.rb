69d80f2bc3f54d7519c1acbbe6c1eaae3e1195b8
Blake Mizerany <blake.mizerany@gmail.com>
2011-04-27 14:40:43 -0700

    update example/set

--- example/set.rb
+++ example/set.rb
@@ -2,8 +2,11 @@ require 'rubygems'
 require 'eventmachine'
 require 'fraggle'
 
+MaxInt64 = (1<<62)-1
+
 EM.run do
   c = Fraggle.connect
+  c.log.level = Logger::DEBUG
 
   EM.add_periodic_timer(1) do
     c.get("/hello") do |e|
@@ -11,7 +14,7 @@ EM.run do
     end
   end
 
-  c.set('/hello', 'world', 0) do |e|
-    p e
+  c.set('/hello', 'world', MaxInt64) do |e|
+    p [:set, e]
   end
 end
